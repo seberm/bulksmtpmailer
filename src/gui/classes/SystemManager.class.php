04cb6a135369513b0db48c3272aa9a83cd059106
Sabart Otto <seberm@gmail.com>
2010-11-27 13:01:01 +0100

    Implemented system options administration

--- src/gui/classes/SystemManager.class.php
+++ src/gui/classes/SystemManager.class.php
@@ -11,59 +11,49 @@ class SystemManager extends AdminModule {
 
 	public function getContent () {
 		
-		$action = isset($_GET['action']) ? $_GET['action'] : "showList";
+		$action = isset($_GET['action']) ? $_GET['action'] : "show";
 
 		switch ($action) {
-			case "showForm":
-				$output = $this->showForm();
-				break;
-
-			case "delete":
-				$output = $this->delete();
-				break;
 
 			case "save":
 				$output = $this->save();
 				break;
 
-			case "showList":
+			case "showForm":
 			default:
-				$output = $this->showList();
+				$output = $this->showForm();
 				break;
 		}
 
 		return $output;
 	}
-
+	
 
 	private function showForm () {
 		
 		global $_MySql;
 
-		$contentId = isset($_GET['id']) ? $_GET['id'] : 0;
-
-		if (is_numeric($contentId) && ($contentId != 0)) {
-			$sql = "SELECT * FROM `Mail` WHERE `id` = ".$contentId.";";
-			$res = $_MySql->query($sql);
-			$row = $res->fetch_assoc();
-		} else $row = Array();
+		$sql = "SELECT * FROM `SystemSettings`";
+		$res = $_MySql->query($sql);
 
 		$output = "
 		<form method=\"post\" action=\"?module=".get_class($this)."&amp;action=save\" enctype=\"multipart/form-data\">
 		<table border=\"0\" cellspacing=\"5\">
 			<tr>
-				<td>Name:</td>
-				<td><input type=\"text\" name=\"name\" value=\"".(isset($row['Name']) ? $row['Name'] : "")."\" /></td>
-			</tr>
-			<tr>
-				<td>E-mail:</td>
-				<td><input type=\"text\" name=\"email\" value=\"".(isset($row['Email']) ? $row['Email'] : "@")."\" /></td>
-			</tr>
-		 ";
-
+				<th>Name</th>
+				<th>Value</th>
+			</tr>";
+		
+		while ($row = $res->fetch_assoc()) {
+			
+			$output .= "<tr>";
+				$output .= "<td>".$row['Name'].":</td>";
+				$output .= "<td><input type=\"text\" name=\"Items[".$row['Item']."]\" value=\"".(isset($row['Value']) ? $row['Value'] : "")."\" /></td>";
+			$output .= "</tr>";
+		}
+		
 		$output .= "</table>
 		<input type=\"submit\" value=\"Save\" /><a href=\"?module=".get_class($this)."\"><input type=\"button\" value=\"Back\" /></a>
-		<input type=\"hidden\" name=\"contentId\" value=\"".$contentId."\" />
 		</form>
 		";
 
@@ -71,83 +61,33 @@ class SystemManager extends AdminModule {
 	}
 
 
-	private function delete () {
-		
-		global $_MySql;
-
-		if (isset($_GET['id']))
-			$contentId = intval($_GET['id']);
-		else return "<div class=\"error\">Bad object ID</div>";
-
-		$sql = "DELETE FROM `Mail` WHERE `id` = ".$contentId.";";
-		
-		if ($_MySql->query($sql)) {
-			
-			$output = "<div class=\"success\">Successfully removed</div>";
-			$output .= $this->showList();
-			return $output;
-		} else return "<div class=\"error\">Deletion error</div>";
-	}
-
-
 	private function save () {
 		
 		global $_MySql;
 
-		$contentId = isset($_POST['contentId']) ? intval($_POST['contentId']) : 0;
-		$name = isset($_POST['name']) ? $_POST['name'] : "";
-		$email = isset($_POST['email']) ? $_POST['email'] : "";
+		$items = isset($_POST['Items']) ? $_POST['Items'] : Array();
 		
-		if (!Utils::isEmail($email)) {
+		$sql = "INSERT INTO `SystemSettings` (`Item`, `Value`) VALUES ";
+		
+		$values = "";
+		foreach ($items as $key => $value) {
+			// $key = DB.Item
+			// $value = DB.Value
 			
-			$output = "<div class=\"error\">Bad e-mail format</div>";
-			$output .= $this->showForm();
+			if (!empty($values))
+				$values .= ", ";
 			
-			return $output;
+			$values .= "('".$key."', '".$value."')";
 		}
-
-		$sql = "INSERT INTO `Mail` (`id`, `Name`, `Email`)
-				VALUES (".$contentId.", '".$name."', '".$email."')
-				ON DUPLICATE KEY UPDATE `Name` = '".$name."', `Email` = '".$email."';";
+		
+		$sql .= $values." ON DUPLICATE KEY UPDATE `Value` = VALUES(`Value`);";
 		$db = $_MySql->query($sql);
-
-		if ($contentId == 0)
-			$contentId = $_MySql->insert_id;
-
 		
 		if ($db)
 			$output = "<div class=\"success\">Data saved</div>";
 		else $output = "<div class=\"error\">Saving error</div>";
 
-		$output .= $this->showList();
-
-		return $output;
-	}
-
-
-	private function showList () {
-		
-		global $_MySql;
-
-		$output = "<span class=\"newItem\"><a href=\"?module=".get_class($this)."&amp;action=showForm\"><img src=\"".CURRENT_ROOT."gui/images/tools/add.png\" alt=\"Add\" title=\"Add\" /></a></span>";
-		$output .= "<br /><br />";
-		$output .= "<table class=\"listTable\" cellpadding=\"5\" cellspacing=\"0\" border=\"0\">
-						<tr>
-							<th>Name</th>
-							<th>E-mail</th>
-							<th>Action</th>
-						</tr>";
-
-		$sql = "SELECT `id`, `Name`, `Email` FROM `Mail` ORDER BY `id` ASC;";
-		$res = $_MySql->query($sql);
-
-		while ($row = $res->fetch_assoc()) {
-			$actions = "<a href=\"?module=".get_class($this)."&amp;action=showForm&amp;id=".$row['id']."\"><img src=\"".CURRENT_ROOT."gui/images/tools/edit.png\" title=\"Edit\" alt=\"Edit\" /></a>";
-			$actions .= "<a href=\"?module=".get_class($this)."&amp;action=delete&amp;id=".$row['id']."\"><img src=\"".CURRENT_ROOT."gui/images/tools/remove.png\" title=\"Remove\" alt=\"Remove\" class=\"remove\" /></a>";
-			$output .= "<tr><td>".$row['Name']."</td><td>".$row['Email']."</td><td align=\"center\">".$actions."</td></tr>";
-		}
-
-		$output .= "</table>";
+		$output .= $this->showForm();
 
 		return $output;
 	}
--- tables.sql
+++ tables.sql
@@ -24,7 +24,7 @@ INSERT INTO `Message` (`Subject`, `Text`) VALUES
 
 
 CREATE TABLE IF NOT EXISTS 	`System` (
-	`Item` varchar(100) COLLATE utf8_czech_ci NOT NULL,
+	`Item` varchar(50) COLLATE utf8_czech_ci NOT NULL,
 	`Value` int(10) unsigned NOT NULL,
 	UNIQUE KEY (`Item`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
@@ -32,3 +32,31 @@ CREATE TABLE IF NOT EXISTS 	`System` (
 INSERT INTO `System` (`Item`, `Value`) VALUES
 ('SendingMessageID', 0),
 ('ActiveSending', false);
+
+
+
+
+
+/* GUI tables */
+CREATE TABLE IF NOT EXISTS 	`SystemSettings` (
+	`Item` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+	`Value` varchar(100) COLLATE utf8_czech_ci NOT NULL,
+	`Name` varchar(50) COLLATE utf8_czech_ci NOT NULL,
+	`Description` text COLLATE utf8_czech_ci NOT NULL,
+	UNIQUE KEY (`Item`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_czech_ci;
+
+INSERT INTO `SystemSettings` (`Item`, `Value`, `Name`, `Description`) VALUES
+('bound', 'science-agency.cz', 'Boundary', ''),
+('mailer', 'phpSMTPBulk', 'Mailer identification', ''),
+('charset', 'utf-8', 'Charset', ''),
+('from', 'somebody@server.tld', 'From', 'Here is the e-mail adress of sender.'),
+('contentType', 'html', 'Content type', 'If you want to use HTML in your messages you must choose the HTML option here, if you don''nt you should choose the plaintext option.'),
+('smtpServer', 'server.tld', 'SMTP server', ''),
+('smtpLogin', 'login_name', 'SMTP login', ''),
+('smtpPassword', 'password', 'SMTP password', ''),
+('smtpPort', '25', 'SMTP Port', 'The default value is port number 25.'),
+('smtpTimeout', '30', 'SMTP timeout', 'The default value is 30.'),
+('smtpAuthType', 'login', 'SMTP Authorization type', 'The default value is "login".'),
+('smtpSmtpType', 'smtp', 'SMTP Type', 'The default is "esmtp". You can choose the "smtp" value too.'),
+('batch', '50', 'Batch', 'Number of e-mails which you want to send in one batch.');
